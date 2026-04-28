db4b85ff3bfc98ef38e9c2b29ba715b4abdb0d25
Ankit0581206 <ankarn07@gmail.com>
2026-04-28 14:42:37 +0545

    Add URL model and database connection

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
 const url_model = require("./models/shorturl.js");
 const app = express();
 
@@ -20,34 +19,10 @@ const connectMongoDB = async (mongoURI) => {
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
 
 app.get("/", async (req, res) => {
   const urlLists = await url_model.find();
   res.render("index", { shorturls: urlLists });
-});
-
-app.post("/shorturls", async (req, res) => {
-  await url_model.create({ full: req.body.fullUrl });
-  res.redirect("/");
-});
-
-app.get("/:shorturl", async (req, res) => {
-  const shorturl = await url_model.findOne({ short: req.params.shorturl });
-  if (shorturl == null) return res.sendStatus(404);
-
-  shorturl.clicks++;
-  shorturl.save();
-
-  res.redirect(shorturl.full);
-});
-
-app.post("/:id", async (req, res) => {
-  const id = req.params.id;
-  const deleteAction = await url_model.findOneAndDelete({ _id: id });
-  if (deleteAction == null) return res.sendStatus(404);
-
-  res.redirect("/");
 });
 
 app.listen(PORT, () => {
